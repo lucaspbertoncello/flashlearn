46aefff3b1cb6074d81ed377a237196e03100863
Lucas <lucasbertoncello1@gmail.com>
2025-04-02 15:26:51 -0300

    feat: flashcards endpoints

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 
 const DeckController = require("./app/controllers/DeckController");
+const CardController = require("./app/controllers/CardController");
 
 const router = Router();
 
@@ -9,5 +10,11 @@ router.get("/decks/:id", DeckController.show);
 router.post("/decks", DeckController.store);
 router.put("/decks/:id", DeckController.update);
 router.delete("/decks/:id", DeckController.delete);
+
+router.get("/flashcards", CardController.index);
+router.get("/flashcards/:id", CardController.show);
+router.post("/flashcards", CardController.store);
+router.put("/flashcards", CardController.update);
+router.delete("/flashcards", CardController.delete);
 
 module.exports = router;
